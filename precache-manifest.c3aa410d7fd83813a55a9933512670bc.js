5fffa6783f48d1562f78287fe130e7ae720c311e
Judy Zhang <jzhang@Judys-MacBook-Air.local>
2020-08-17 02:12:44 -0700

    Updates

---
+++ precache-manifest.c3aa410d7fd83813a55a9933512670bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22343da0a3c815a12893923998cba237",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "baefabc498c4f2a7d2a0",
+    "url": "/covidtracker/static/css/main.3efda88d.chunk.css"
+  },
+  {
+    "revision": "7172acc67fae4b900362",
+    "url": "/covidtracker/static/js/2.0a3500b7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.0a3500b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baefabc498c4f2a7d2a0",
+    "url": "/covidtracker/static/js/main.678831ca.chunk.js"
+  },
+  {
+    "revision": "abb288adae797851ddc5",
+    "url": "/covidtracker/static/js/runtime-main.345c4fa2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/image.d7265326.png"
+  }
+]);
